21999f0789580bf465838a0aaf7b1074ea434d09
Alexey Golodenko <alexey.golodenko@gmail.com>
2022-01-31 12:50:30 +0200

    fix-perfomance

---
+++ js/scripts.js
@@ -0,0 +1,94 @@
+// mobile-menu
+function openMenu() {
+   document.getElementById('mobile-menu').classList.toggle('open');
+   document.getElementById('body').classList.toggle('active');
+   document.getElementById('overlay').classList.toggle('open');
+}
+
+// slider
+const swiper = new Swiper('.hero__slider', {
+
+   autoplay: {
+      delay: 1000,
+      stopOnLastSlide: false,
+      disableOnInteraction: false,
+   },
+
+   pagination: {
+      el: '.hero__slider--pagination',
+      clickable: true,
+   },
+
+   grabCursor: true,
+   slidesPerView: 1.5,
+   //initialSlide: 1,
+   watchOverflow: true,
+   spaceBetween: 20,
+   //centeredSlides: true,
+   //loop: true,
+
+   speed: 800,
+
+   preloadImages: false,
+   lazy: {
+      loadOnTransitionStart: false,
+      loadPrevNext: false,
+   },
+
+   watchSlidesProgres: true,
+   watchSlidesVisibility: true,
+
+   breakpoints: {
+      768: {
+         slidesPerView: 2,
+         centeredSlides: false,
+      },
+      1024: {
+         slidesPerView: 1.85,
+         centeredSlides: false,
+      },
+
+      1280: {
+         slidesPerView: 2.45,
+         centeredSlides: false,
+      },
+
+      1366: {
+         slidesPerView: 2.7,
+         centeredSlides: false,
+      },
+
+      1440: {
+         slidesPerView: 2.85,
+         centeredSlides: false,
+      },
+
+      1900: {
+         slidesPerView: 3.5,
+         centeredSlides: false,
+      },
+   },
+});
+
+// show-hide
+function openSectionWatched() {
+   document.getElementById('showWatched').classList.toggle('open');
+}
+
+function openSectionSale() {
+   document.getElementById('showSale').classList.toggle('open');
+}
+
+function openSectionTopSales() {
+   document.getElementById('showTopSales').classList.toggle('open');
+}
+
+function openSectionAbout() {
+   document.getElementById('showAbout').classList.toggle('open');
+}
+
+// switch
+function clickSwitch() {
+   document.getElementById('ru').classList.toggle('active');
+   document.getElementById('ua').classList.toggle('no-active');
+}
